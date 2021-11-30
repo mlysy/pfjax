fefde6eab633cc0765b8e54a2b270fc84096fea6
mlysy <mlysy@users.noreply.github.com>
2021-11-30 09:27:34 -0500

    revised oop paradigm

--- tests/test_particle_filter.py
+++ tests/test_particle_filter.py
@@ -64,11 +64,13 @@ class TestFor(unittest.TestCase):
         bm_model = bm.BMModel(dt=dt)
         # simulate without for-loop
         y_meas, x_state = pf.meas_sim(bm_model, n_obs, x_init, theta, key)
-        # run particle filter
+        # particle filter specification
         n_particles = 7
         key, subkey = random.split(key)
+        # pf with for-loop
         pf_out1 = pf.particle_filter_for(bm_model,
                                          y_meas, theta, n_particles, subkey)
+        # pf without for-loop
         pf_out2 = pf.particle_filter(
             bm_model, y_meas, theta, n_particles, subkey)
         for k in pf_out1.keys():
@@ -112,10 +114,12 @@ class TestOOP(unittest.TestCase):
         # simulate with oop
         bm_model = bm.BMModel(dt=dt)
         y_meas, x_state = pf.meas_sim(bm_model, n_obs, x_init, theta, key)
-        # run particle filter
+        # particle filter specification
         n_particles = 7
         key, subkey = random.split(key)
+        # pf with globals
         pf_out1 = particle_filter(y_meas, theta, n_particles, subkey)
+        # pf with oop
         pf_out2 = pf.particle_filter(
             bm_model, y_meas, theta, n_particles, subkey)
         for k in pf_out1.keys():
@@ -142,8 +146,13 @@ class TestJit(unittest.TestCase):
         bm_model = bm.BMModel(dt=dt)
         y_meas1, x_state1 = pf.meas_sim(bm_model, n_obs, x_init, theta, key)
         # simulate with jit
-        y_meas2, x_state2 = jax.jit(pf.meas_sim, static_argnums=(0, 1))(
-            bm_model, n_obs, x_init, theta, key)
+        meas_sim_jit = jax.jit(pf.meas_sim, static_argnums=(0, 1))
+        # use wrong dt
+        bm_model2 = bm.BMModel(dt=2.0 * dt)
+        y_meas2, x_state2 = meas_sim_jit(bm_model2, n_obs, x_init, theta, key)
+        # use correct dt
+        bm_model2.dt = dt
+        y_meas2, x_state2 = meas_sim_jit(bm_model2, n_obs, x_init, theta, key)
         self.assertAlmostEqual(rel_err(y_meas1, y_meas2), 0.0)
         self.assertAlmostEqual(rel_err(x_state1, x_state2), 0.0)
 
@@ -160,11 +169,13 @@ class TestJit(unittest.TestCase):
         # simulate data
         bm_model = bm.BMModel(dt=dt)
         y_meas, x_state = pf.meas_sim(bm_model, n_obs, x_init, theta, key)
-        # run particle filter
+        # particle filter specification
         n_particles = 7
         key, subkey = random.split(key)
+        # pf without jit
         pf_out1 = pf.particle_filter(
             bm_model, y_meas, theta, n_particles, subkey)
+        # pf with jit
         pf_out2 = jax.jit(pf.particle_filter, static_argnums=(0, 3))(
             bm_model, y_meas, theta, n_particles, subkey)
         for k in pf_out1.keys():
@@ -206,7 +217,7 @@ if __name__ == '__main__':
 # print("x_state = \n",
 #       jnp.max(jnp.abs(x_state_for - x_state)))
 
-# # run particle filter
+# # particle filter specification
 # n_particles = 7
 # key, subkey = random.split(key)
 # pf_for = particle_filter_for(y_meas, theta, n_particles, subkey)
