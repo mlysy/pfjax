16dd935dbbe0bf75b6ef0b23bda20c1a481e98a8
mohanwu <mohanwu2014@gmail.com>
2022-01-09 18:32:26 -0500

    Updated examples and jitted particle_filter

--- tests/deprecated/particle_filter.py
+++ tests/deprecated/particle_filter.py
@@ -167,7 +167,7 @@ def particle_filter_for(y_meas, theta, n_particles, key):
     }
 
 
-# @partial(jax.jit, static_argnums=2)
+@partial(jax.jit, static_argnums=2)
 def particle_filter(y_meas, theta, n_particles, key):
     """
     Apply particle filter for given value of `theta`.
