972cac301c93f964e64f8839a17560cda44b7ad7
pranavsubramani <pranav0212.s@gmail.com>
2022-06-14 22:55:42 -0400

    removing _get_params from lotvol

--- src/pfjax/models/lotvol_model.py
+++ src/pfjax/models/lotvol_model.py
@@ -1,8 +1,6 @@
 """
 Lotka-Volterra predator-prey model.
-
 The model is:
-
 ```
 exp(x_m0) = exp( (logH_m0, logL_m0) ) ~ pi(x_m0) \propto 1
 logH_mt ~ N(logH_{m,t-1} + (alpha - beta exp(logL_{m,t-1})) dt/m,
@@ -11,28 +9,22 @@ logL_mt ~ N(logL_{m,t-1} + (-gamma + delta exp(logH_{m,t-1})) dt/m,
             sigma_L^2 dt/m)
 y_t ~ N( exp(x_{m,mt}), diag(tau_H^2, tau_L^2) )
 ```
-
 - Model parameters: `theta = (alpha, beta, gamma, delta, sigma_H, sigma_L, tau_H, tau_L)`.
 - Global constants: `dt` and `n_res`, i.e., `m`.
 - State dimensions: `n_state = (n_res, 2)`.
 - Measurement dimensions: `n_meas = 2`.
-
 **Notes:**
 - The measurement `y_t` corresponds to `x_t = (x_{m,(t-1)m+1}, ..., x_{m,tm})`, i.e., aligns with the last element of `x_t`.
 - The prior is such that `p(x_0 | y_0, theta)` is given by:
-
     ```
     x_{m,n} = 0 for n = -m+1, ..., -1,
     exp(x_{m0}) ~ TruncatedNormal( y_0, diag(tau_H^2, tau_L^2) ),
     ```
-
     where
-
     ```
     z ~ TruncatedNormal(mu, diag(sigma^2)) <=>
     z = mu + diag(sigma) Z_0,   Z_0 ~iid N(0,1) truncated at -mu.
     ```
-
 """
 
 import jax
@@ -63,7 +55,6 @@ class LotVolModel(sde.SDEModel):
     def __init__(self, dt, n_res):
         r"""
         Class constructor for the Lotka-Volterra model.
-
         Args:
             dt: SDE interobservation time.
             n_res: SDE resolution number.  There are `n_res` latent variables per observation, equally spaced with interobservation time `dt/n_res`.
@@ -80,15 +71,10 @@ class LotVolModel(sde.SDEModel):
         # than if using prespecified SDE dimensions...
         self._n_state = (self._n_res, 2)
 
-    def _get_params(self, params):
-        params = jnp.exp(params)
-        return params
-
     def drift(self, x, theta):
         r"""
         Calculates the SDE drift function.
         """
-        theta = self._get_params(theta)
         alpha = theta[0]
         beta = theta[1]
         gamma = theta[2]
@@ -100,15 +86,12 @@ class LotVolModel(sde.SDEModel):
         r"""
         Calculates the SDE diffusion function.
         """
-        theta = self._get_params(theta)
         return theta[4:6]
 
     def state_lpdf_for(self, x_curr, x_prev, theta):
         r"""
         Calculates the log-density of `p(x_curr | x_prev, theta)`.
-
         For-loop version for testing.
-
         Args:
             x_curr: State variable at current time `t`.
             x_prev: State variable at previous time `t-1`.
@@ -116,7 +99,6 @@ class LotVolModel(sde.SDEModel):
         Returns:
             The log-density of `p(x_curr | x_prev, theta)`.
         """
-        theta = self._get_params(theta)
         dt_res = self._dt/self._n_res
         x0 = jnp.append(jnp.expand_dims(
             x_prev[self._n_res-1], axis=0), x_curr[:self._n_res-1], axis=0)
@@ -134,18 +116,14 @@ class LotVolModel(sde.SDEModel):
     def state_sample_for(self, key, x_prev, theta):
         r"""
         Samples from `x_curr ~ p(x_curr | x_prev, theta)`.
-
         For-loop version for testing.
-
         Args:
             key: PRNG key.
             x_prev: State variable at previous time `t-1`.
             theta: Parameter value.
-
         Returns:
             Sample of the state variable at current time `t`: `x_curr ~ p(x_curr | x_prev, theta)`.
         """
-        theta = self._get_params(theta)
         dt_res = self._dt/self._n_res
         sigma = theta[4:6] * jnp.sqrt(dt_res)
         x_curr = jnp.zeros(self._n_state)
@@ -156,76 +134,17 @@ class LotVolModel(sde.SDEModel):
                 random.normal(subkey, (self._n_state[1],)) * sigma
             x_curr = x_curr.at[t].set(x_state)
         return x_curr
-
-    def state_lpdf_for(self, x_curr, x_prev, theta):
-        """
-        Calculates the log-density of `p(x_curr | x_prev, theta)`.
-
-        For-loop version for testing.
-
-        Args:
-            x_curr: State variable at current time `t`.
-            x_prev: State variable at previous time `t-1`.
-            theta: Parameter value.
-
-        Returns:
-            The log-density of `p(x_curr | x_prev, theta)`.
-        """
-        theta = self._get_params(theta)
-        dt_res = self._dt/self._n_res
-        x0 = jnp.append(jnp.expand_dims(
-            x_prev[self._n_res-1], axis=0), x_curr[:self._n_res-1], axis=0)
-        x1 = x_curr
-        sigma = theta[4:6] * jnp.sqrt(dt_res)
-        lp = jnp.array(0.0)
-        for t in range(self._n_res):
-            lp = lp + jnp.sum(jsp.stats.norm.logpdf(
-                x1[t],
-                loc=lotvol_drift(x0[t], dt_res, theta),
-                scale=sigma
-            ))
-        return lp
-
-    def state_sample_for(self, key, x_prev, theta):
-        """
-        Samples from `x_curr ~ p(x_curr | x_prev, theta)`.
-
-        For-loop version for testing.
-
-        Args:
-            key: PRNG key.
-            x_prev: State variable at previous time `t-1`.
-            theta: Parameter value.
-
-        Returns:
-            Sample of the state variable at current time `t`: `x_curr ~ p(x_curr | x_prev, theta)`.
-        """
-        theta = self._get_params(theta)
-        dt_res = self._dt/self._n_res
-        sigma = theta[4:6] * jnp.sqrt(dt_res)
-        x_curr = jnp.zeros(self._n_state)
-        x_state = x_prev[self._n_res-1]
-        for t in range(self._n_res):
-            key, subkey = random.split(key)
-            x_state = lotvol_drift(x_state, dt_res, theta) + \
-                random.normal(subkey, (self._n_state[1],)) * sigma
-            x_curr = x_curr.at[t].set(x_state)
-        return x_curr
-
 
     def meas_lpdf(self, y_curr, x_curr, theta):
         r"""
         Log-density of `p(y_curr | x_curr, theta)`.
-
         Args:
             y_curr: Measurement variable at current time `t`.
             x_curr: State variable at current time `t`.
             theta: Parameter value.
-
         Returns
             The log-density of `p(y_curr | x_curr, theta)`.
         """
-        theta = self._get_params(theta)
         tau = theta[6:8]
         return jnp.sum(
             jsp.stats.norm.logpdf(y_curr,
@@ -235,16 +154,13 @@ class LotVolModel(sde.SDEModel):
     def meas_sample(self, key, x_curr, theta):
         r"""
         Sample from `p(y_curr | x_curr, theta)`.
-
         Args:
             key: PRNG key.
             x_curr: State variable at current time `t`.
             theta: Parameter value.
-
         Returns:
             Sample of the measurement variable at current time `t`: `y_curr ~ p(y_curr | x_curr, theta)`.
         """
-        theta = self._get_params(theta)
         tau = theta[6:8]
         return jnp.exp(x_curr[-1]) + \
             tau * random.normal(key, (self._n_state[1],))
@@ -252,19 +168,15 @@ class LotVolModel(sde.SDEModel):
     def pf_init(self, key, y_init, theta):
         r"""
         Importance sampler for `x_init`.  
-
         See file comments for exact sampling distribution of `p(x_init | y_init, theta)`, i.e., we have a "perfect" importance sampler with `logw = CONST(theta)`.
-
         Args:
             key: PRNG key.
             y_init: Measurement variable at initial time `t = 0`.
             theta: Parameter value.
-
         Returns:
             - x_init: A sample from the proposal distribution for `x_init`.
             - logw: The log-weight of `x_init`.
         """
-        theta = self._get_params(theta)
         tau = theta[6:8]
         key, subkey = random.split(key)
         x_init = jnp.log(y_init + tau * random.truncated_normal(
