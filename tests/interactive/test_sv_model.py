88b188a72456fcc46d7acc057097cff9a5439b13
mlysy <mlysy@users.noreply.github.com>
2025-06-02 09:08:37 -0400

    updated StoVolModel prototype

--- tests/interactive/test_sv_model.py
+++ tests/interactive/test_sv_model.py
@@ -3,23 +3,22 @@ import jax.numpy as jnp
 
 
 class StoVolModel(sde.SDEModel):
-    def __init__(self, dt):
-        super().__init__(dt, n_res=2, diff_diag=False)
+    def __init__(self, n_res, dt, x_init, eps=0.00001):
+        """
+        Parameters
+        ----------
+        x_init: jax.Array
+            Vector of length two specifying the initial value of the SDE (considered fixed and known).
+        """
+        super().__init__(dt, n_res, diff_diag=False, bootstrap=False)
+        self._eps = eps
 
     def drift(self, x, theta):
-        alpha = theta[0]
-        gamma = theta[1]
-        eta = theta[2]
-        sigma = theta[3]
-        rho = theta[4]
+        alpha, gamma, eta, sigma, rho = theta
         return jnp.array([alpha - 0.5 * jnp.exp(2 * x[1]), -gamma * x[1] + eta])
 
     def diff(self, x, theta):
-        alpha = theta[0]
-        gamma = theta[1]
-        eta = theta[2]
-        sigma = theta[3]
-        rho = theta[4]
+        alpha, gamma, eta, sigma, rho = theta
         return jnp.array(
             [
                 [jnp.exp(2 * x[1]), rho * jnp.exp(x[1]) * sigma],
@@ -27,17 +26,33 @@ class StoVolModel(sde.SDEModel):
             ]
         )
 
+    def bridge_pars(self, y_curr, theta):
+        A = jnp.array([1.0, 0.0])
+        Omega = eps**2
+        Y = y_curr
+        return A, Omega, Y
+
+    def meas_pars(self, x_curr, theta):
+        A, Omega, _ = self.bridge_pars(y_curr=None, theta=theta)
+        mu = jnp.dot(A, x_curr[-1])
+        sigma = jnp.sqrt(Omega)
+        return mu, sigma
+
     def meas_sample(self, key, x_curr, theta):
         # just return the last log-asset value
         # x_curr is an array of shape `(n_res, 2)`.
-        return x_curr[-1, 0]
+        mu, sigma = self.meas_pars(x_curr=x_curr, theta=theta)
+        return mu + sigma * jax.random.normal(key)
 
-    def meas_lpdf(self, y_meas, x_curr, theta):
-        delta = jnp.abs(y_meas - x_curr[-1, 0])
-        # method 1: with error checking
-        return jnp.where(delta > 1e-10, -jnp.inf, 0.0)
-        # method 2: without error checking (hope for the best)
-        return 0.0
+    def meas_lpdf(self, x_curr, y_curr, theta):
+        mu, sigma = self.meas_pars(x_curr=x_curr, theta=theta)
+        return jsp.stats.norm.logpdf(x=y_curr, loc=mu, scale=sigma)
+
+    def pf_step(self, key, x_prev, y_curr, theta):
+        A, Omega, Y = self.bridge_pars(y_curr=y_curr, theta=theta)
+        return self.bridge_step(
+            key=key, x_prev=x_prev, y_curr=y_curr, theta=theta, Y=Y, A=A, Omega=Omega
+        )
 
 
 # true parameters
