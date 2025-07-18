e96074750a1817de4c9f267f7c0f67213f6fdf99
mlysy <mlysy@users.noreply.github.com>
2025-07-18 09:30:12 -0400

    changed argument 'x' to 'tree' in custom tree utils

--- src/pfjax/utils.py
+++ src/pfjax/utils.py
@@ -3,10 +3,10 @@ Utility functions.
 """
 
 import jax
+import jax.flatten_util as jfu
 import jax.numpy as jnp
 import jax.scipy as jsp
 import jax.tree_util as jtu
-import jax.flatten_util as jfu
 
 
 def logw_to_prob(logw):
@@ -33,7 +33,8 @@ def rm_keys(x, keys):
 
 # --- tree helper functions ----------------------------------------------------
 
-def tree_array2d(x, shape0=None):
+
+def tree_array2d(tree, shape0=None):
     r"""
     Convert a PyTree into a 2D JAX array.
 
@@ -44,8 +45,8 @@ def tree_array2d(x, shape0=None):
     - This function returns a tuple containing a Callable, so can't be jitted directly.  Can however be called in jitted code so long as the output is a PyTree.
 
     Args:
-        x: A Pytree.
-        shape0: Optional value of the leading dimension.  If `None` is deduced from `x`.
+        tree: A Pytree.
+        shape0: Optional value of the leading dimension.  If `None` is deduced from `tree`.
 
     Returns:
         tuple:
@@ -53,10 +54,13 @@ def tree_array2d(x, shape0=None):
         - **unravel_fn** - A Callable to reconstruct the original PyTree.
     """
     if shape0 is None:
-        shape0 = jtu.tree_leaves(x)[0].shape[0]  # leading dimension
-    y, _unravel_fn = jfu.ravel_pytree(x)
+        shape0 = jtu.tree_leaves(tree)[0].shape[0]  # leading dimension
+    y, _unravel_fn = jfu.ravel_pytree(tree)
     y = jnp.reshape(y, (shape0, -1))
-    def unravel_fn(array2d): return _unravel_fn(jnp.ravel(array2d))
+
+    def unravel_fn(array2d):
+        return _unravel_fn(jnp.ravel(array2d))
+
     return y, unravel_fn
 
 
@@ -64,7 +68,7 @@ def tree_add(tree1, tree2):
     r"""
     Add two pytrees leafwise.
     """
-    return jtu.tree_map(lambda x, y: x+y, tree1, tree2)
+    return jtu.tree_map(lambda x, y: x + y, tree1, tree2)
     # return jtu.tree_map(lambda x, y: x+y[0], tree1, (tree2,))
 
 
@@ -74,15 +78,12 @@ def tree_mean(tree, logw):
     """
     prob = logw_to_prob(logw)
     broad_mult = jtu.Partial(jax.vmap(jnp.multiply), prob)
-    return jtu.tree_map(
-        jtu.Partial(jnp.sum, axis=0),
-        jtu.tree_map(broad_mult, tree)
-    )
+    return jtu.tree_map(jtu.Partial(jnp.sum, axis=0), jtu.tree_map(broad_mult, tree))
 
 
-def tree_shuffle(tree, index):
+def tree_subset(tree, index):
     """
-    Shuffle the leading dimension of each leaf of a pytree by values in index.
+    Subset the leading dimension of each leaf of a pytree by values in index.
     """
     return jtu.tree_map(lambda x: x[index, ...], tree)
 
@@ -94,36 +95,51 @@ def tree_zeros(tree):
     return jtu.tree_map(lambda x: jnp.zeros_like(x), tree)
 
 
-def tree_remove_last(x):
+def tree_remove_last(tree):
     """
     Remove last element of each leaf of pytree.
     """
-    return jtu.tree_map(lambda _x: _x[:-1], x)
+    return jtu.tree_map(lambda _x: _x[:-1], tree)
 
 
-def tree_remove_first(x):
+def tree_remove_first(tree):
     """
     Remove first element of each leaf of pytree.
     """
-    return jtu.tree_map(lambda _x: _x[1:], x)
+    return jtu.tree_map(lambda _x: _x[1:], tree)
 
 
-def tree_keep_last(x):
+def tree_keep_last(tree):
     """
     Keep only last element of each leaft of pytree.
     """
-    return jtu.tree_map(lambda _x: _x[-1], x)
+    return jtu.tree_map(lambda _x: _x[-1], tree)
 
 
-def tree_append_first(x, first):
+def tree_append_first(tree, first):
     """
-    Append `first` to start of each leaf of `x` along 1st dimension.
+    Append `first` to start of each leaf of `tree` along 1st dimension.
     """
-    return jtu.tree_map(lambda x0, _x: jnp.concatenate([x0[None], _x]), first, x)
+    return jtu.tree_map(lambda x0, _x: jnp.concatenate([x0[None], _x]), first, tree)
 
 
-def tree_append_last(x, last):
+def tree_append_last(tree, last):
     """
-    Append `last` to end of each leaf of `x` along 1st dimension.
+    Append `last` to end of each leaf of `tree` along 1st dimension.
     """
-    return jtu.tree_map(lambda xl, _x: jnp.concatenate([_x, xl[None]]), last, x)
+    return jtu.tree_map(lambda xl, _x: jnp.concatenate([_x, xl[None]]), last, tree)
+
+
+# def tree_leading_dim(tree):
+#     """
+#     Compute the leading dimension of `tree`.
+
+#     If this isn't the same for each leaf, return jnp.nan.  A consequence though
+#     is that return type is float instead of int.
+
+#     Also, not sure what to do about gradients through this...
+#     """
+#     leading = jtu.tree_map(lambda x: jnp.atleast_1d(x).shape[0], tree)
+#     leading_min = jtu.tree_reduce(lambda x, y: jnp.minimum(x, y), leading)
+#     leading_max = jtu.tree_reduce(lambda x, y: jnp.maximum(x, y), leading)
+#     return jnp.where(leading_min < leading_max, jnp.nan, leading_min)
