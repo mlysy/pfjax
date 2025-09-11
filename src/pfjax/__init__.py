1a1ed80ba63a4b901e32a4e691450f28dbd34a11
mlysy <mlysy@users.noreply.github.com>
2025-09-11 10:12:05 -0400

    added __all__ to flattened imports (pfjax, pfjax/models, pfjax/test/models)

--- src/pfjax/__init__.py
+++ src/pfjax/__init__.py
@@ -1,10 +1,18 @@
-from .simulate import simulate
 from .loglik_full import loglik_full
-from .particle_filter import particle_filter
-from .particle_filter import particle_filter_rb
-from .particle_smooth import particle_smooth
 from .models.base_model import BaseModel
+from .particle_filter import particle_filter, particle_filter_rb
+from .particle_smooth import particle_smooth
+from .simulate import simulate
 
 # from . __metadata__ import __version__, __author__
 __version__ = "0.0.2"
 __author__ = "Martin Lysy, Pranav Subramani, Jonathan Ramkissoon, Mohan Wu, Michelle Ko, Kanika Choptra"
+
+__all__ = [
+    "BaseModel",
+    "simulate",
+    "loglik_full",
+    "particle_filter",
+    "particle_filter_rb",
+    "particle_smooth",
+]
--- src/pfjax/models/__init__.py
+++ src/pfjax/models/__init__.py
@@ -1,3 +1,6 @@
+from .base_model import BaseModel
 from .bm_model import BMModel
 from .lotvol_model import LotVolModel
 from .pgnet_model import PGNETModel
+
+__all__ = ["BaseModel", "BMModel", "LotVolModel", "PGNETModel"]
