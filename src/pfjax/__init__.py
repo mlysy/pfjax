79c6f3f96cdcb151f98a0836293cbced35f803aa
mlysy <mlysy@users.noreply.github.com>
2025-10-16 17:12:21 -0400

    updated author list and version number

--- src/pfjax/__init__.py
+++ src/pfjax/__init__.py
@@ -5,8 +5,8 @@ from .particle_smooth import particle_smooth
 from .simulate import simulate
 
 # from . __metadata__ import __version__, __author__
-__version__ = "0.0.2"
-__author__ = "Martin Lysy, Pranav Subramani, Jonathan Ramkissoon, Mohan Wu, Michelle Ko, Kanika Choptra"
+__version__ = "0.0.3rc1"
+__author__ = "Martin Lysy, Pranav Subramani, Jonathan Ramkissoon, Mohan Wu, Michelle Ko, Kanika Choptra, Feiyu Zhu, Micky Liu, Monica Zhu"
 
 __all__ = [
     "BaseModel",
