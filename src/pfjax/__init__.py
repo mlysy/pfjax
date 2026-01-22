32725e80b0afa90eb642b6f6b30a019d7f9fc16c
mlysy <mlysy@users.noreply.github.com>
2026-01-22 15:03:17 -0500

    hard-coded version number in pyproject.toml instead of __init__.py

--- src/pfjax/__init__.py
+++ src/pfjax/__init__.py
@@ -1,11 +1,19 @@
+# from . __metadata__ import __version__, __author__
+from importlib.metadata import PackageNotFoundError, version
+
 from .loglik_full import loglik_full
 from .models.base_model import BaseModel
 from .particle_filter import particle_filter, particle_filter_rb
 from .particle_smooth import particle_smooth
 from .simulate import simulate
 
-# from . __metadata__ import __version__, __author__
-__version__ = "0.0.3rc1"
+# __version__ = "0.0.3rc1"
+try:
+    __version__ = version("pfjax")
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "unknown"
+
 __author__ = "Martin Lysy, Pranav Subramani, Jonathan Ramkissoon, Mohan Wu, Michelle Ko, Kanika Choptra, Feiyu Zhu, Micky Liu, Monica Zhu"
 
 __all__ = [
