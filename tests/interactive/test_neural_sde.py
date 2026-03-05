cf92309c1ee68e53712c42b40ec335e80f8ddcc7
mlysy <mlysy@users.noreply.github.com>
2026-03-05 12:58:15 -0500

    test: Updated prototype for NeuralSDE

--- tests/interactive/test_neural_sde.py
+++ tests/interactive/test_neural_sde.py
@@ -14,47 +14,43 @@ import pfjax
 #        return f_impl(x, theta)
 
 
-class DriftDiff(eqx.Module):
+class SDEParams(eqx.Module):
     # eqx.Module wants us to think about NN(x, theta)
     # neural network for drift
-    layers: tuple
+    drift_layers: tuple
     # simple diagonal for diffusion
     diff_sd: jax.Array
 
     def __init__(self, key):
-        key1, key2 = jax.random.split(key)
-        self.layers = (
-            eqx.nn.Linear(2, 8, key=key1),
-            jax.nn.relu,
-            eqx.nn.Linear(8, 2, key=key2),
-        )
-
-    def drift(self, x):
-        for layer in self.layers:
-            x = layer(x)
-        return x
-
-    def diff(self, x):
-        return diff_sd
+        drift_layer_dims = (2, 8)  # can make this an input
+        n_dims = drift_layer_dims[0]  # dimension of SDE
+        n_layers = len(drift_layer_dims)  # number of layers
+        drift_layer_dims += (n_dims,)  # output_dim = input_dim
+        keys = jax.random.split(key, num=n_layers + 1)
+        self.layers = tuple()
+        for l in range(n_layers):
+            self.layers += (
+                eqx.nn.Linear(
+                    in_features=drift_layers_dims[l],
+                    out_features=drift_layers_dims[l + 1],
+                    key=keys[l],
+                ),
+                jax.nn.relu,
+            )
+        self.diff_sd = jax.random.normal(key=keys[-1], shape=(n_dims,))
+        self.diff_sd = jnp.exp(diff_sd)
 
 
 class NeuralSDE(SDEModel):
     def drift(self, x, theta):
         """
         Define the drift function.
-
-        theta: object of class DriftDiff
         """
-        return theta.drift(x)
+        layers = theta.layers
+        for layer in self.layers:
+            x = layer(x)
+        return x
 
     def diff(self, x, theta):
-        return theta.diff(x)
-
-
-model = NeuralSDE(
-    dt,
-    n_res,
-    diag,
-    drift_net,
-    diff_net,
-)
+        diff_sd = theta.diff_sd
+        return diff_sd
